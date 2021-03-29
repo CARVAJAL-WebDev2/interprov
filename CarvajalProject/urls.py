63cd7caaddc3fe2ff7c4c0493d14042431d99195
superpaeng97 <superpaeng97@users.github.com>
2021-03-29 15:02:47 +0800

    my first unit test passed

--- CarvajalProject/urls.py
+++ CarvajalProject/urls.py
@@ -1,3 +1,9 @@
+from django.conf.urls import url
+from OpenTourList import views
+
+urlpatterns = [
+	url(r'^$', views.MainPage, name='mainpage'),
+]
 """CarvajalProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -20,12 +26,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 """
-
-from django.conf.urls import url
-from OpenTourList import views
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-	url(r'^$', views.MainPage, name='mainpage'),
-]
