9c1be4022a7ec22ad7309f9a7cb541f2396dffb0
superpaeng97 <superpaeng97@users.github.com>
2021-04-05 20:21:29 +0800

    Passing UnitTest and Functional Test with Views -rjc

--- OpenTourList/tests.py
+++ OpenTourList/tests.py
@@ -7,3 +7,8 @@ class HomePageTest(TestCase):
 	def test_root_url_resolves_to_mainpage_view(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'mainpage.html')
+
+	def test_save_POST_request(self):
+		response = self.client.post('/', data = {'idName': 'newTouristName'})
+		self.assertIn('newTouristName', response.content.decode())
+		self.assertTemplateUsed(response, 'mainpage.html')
--- OpenTourList/views.py
+++ OpenTourList/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
 
 def MainPage(request):
-	return render(request,'mainpage.html')
+	return render(request,'mainpage.html',{'newTouristName': request.POST.get('idName'),'newSwabCode': request.POST.get('idUniCode'),'newDestination': request.POST.get('idLocEntry1',''),})
